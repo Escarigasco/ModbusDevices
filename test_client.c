c75cc814229b1122f13383ec31e1c143eb4e5204
Federico Zarelli <Federico>
2018-03-04 22:15:06 +0100

    split readers

--- test_client.c
+++ test_client.c
@@ -13,39 +13,15 @@
 
 
 
-int main(int argc, char **argv)
+int main(void)
 {
 
-
-
-
-
     int error_checker=0;
-
-
     const int baud=19200;
     const char parity='N';
     const int stop_bit=1;
     const int no_of_bit=8;
-
-
-    int i=0;
-    int g=0;
-
-   
-
-    //https://github.com/stephane/libmodbus - Readme with modbus functions descritpions
-    /***Modbus Master Init***/
-    modbus_t* ctx;
-    ctx = ctx = modbus_new_tcp("192.168.1.4", 502);
- 
-    
-     
-    /***(1) Start Monitoring - check operating conditions***/
-
-    while(1) { //(1) Start Monitoring
- 
-	modbus_t *ctx;
+    modbus_t *ctx;
 	uint16_t tab_reg[1]={0};
 	uint16_t inp_tab_reg[1]={0};
 	uint8_t tab_bit[1]={0};
@@ -53,34 +29,48 @@ int main(int argc, char **argv)
 	int nPort = 80;
 	int rc;
 	int i=0;
+    int g=0;
 
-	ctx = modbus_new_tcp("127.0.0.1", nPort);
+	ctx = modbus_new_tcp("192.168.1.1", nPort);
+
+
+
+   
+
+    //https://github.com/stephane/libmodbus - Readme with modbus functions descritpions
+ 
+    while(1) { //(1) Start Monitoring
+
+
 	if (modbus_connect(ctx) == -1) {
 	    fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
 	    modbus_free(ctx);
 	    return -1;
-	}
-	for (i=0;i<100;i++){
-		rc = modbus_read_registers(ctx, i, 1, tab_reg);
-		rc = modbus_read_input_bits(ctx, i, 1, inp_tab_bit);
-		rc = modbus_read_input_registers(ctx, i, 1, inp_tab_reg);
-		rc = modbus_read_bits(ctx, i, 1, tab_bit);
-		printf("Input bit %d: %d\n ",i,&inp_tab_bit);
-		printf("Bit %d: %d\n ",i,&tab_bit);
-		printf("Input register %d: %d\n ",i,&inp_tab_reg);
-		printf("Input register %d: %d\n ",i,&tab_reg);
+    }
 
+	for (i=0;i<100;i++){
+		rc = modbus_read_input_bits(ctx, i, 1, inp_tab_bit);
+		printf("Input bit %d: %d\n ",i,&inp_tab_bit);
+	}
+    
+	for (i=10000;i<10100;i++){
+	    rc = modbus_read_bits(ctx, i, 1, tab_bit);
+	    printf("Bit %d: %d\n ",i,&tab_bit);
+	}
+    	for (i=30000;i<30100;i++){
+	    rc = modbus_read_input_registers(ctx, i, 1, inp_tab_reg);
+	    printf("Input register %d: %d\n ",i,&inp_tab_reg);
+	}
+
+    	for (i=40000;i<40100;i++){
+	    rc = modbus_read_registers(ctx, i, 1, tab_reg);
+	    printf("Register %d: %d\n ",i,&tab_reg);
 	}
 
 	if (rc == -1) {
 	    fprintf(stderr, "%s\n", modbus_strerror(errno));
 	    return -1;
 	}
-
-	for (i=0; i < rc; i++) {
-	    printf("reg[%d]=%d (0x%X)\n", i, tab_reg[i], tab_reg[i]);
-	}
-
 
       
  	}
