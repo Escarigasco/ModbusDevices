c06e26ccedb2090bf04df35119ddf4b5bbcbfbc4
Escarigasco <federico.zarelli@aol.com>
2018-07-29 08:30:02 +0000

    mb_mapping dimensions and printf

--- test_server.c
+++ test_server.c
@@ -23,10 +23,10 @@ void update_modbus_server(void)
 	uint8_t bit=1;
 	
 
-	for(i=0; i<99; i++)
+	for(i=0; i<9; i++)
 			{
-					mb_mapping->tab_input_bits[i]=1;
-					mb_mapping->tab_bits[i] = 1;
+					mb_mapping->tab_input_bits[i]=i;
+					mb_mapping->tab_bits[i] = i;
 					mb_mapping->tab_registers[i]=i;
 					mb_mapping->tab_input_registers[i]=i;
 			}
@@ -38,7 +38,9 @@ void update_modbus_server(void)
 
 void* modbus_comms(void *arg)
 	{
-
+	
+	clock_t begin;
+	clock_t end;
 	int socket;
 	modbus_t *ctx;
 	int rc;
@@ -51,20 +53,26 @@ void* modbus_comms(void *arg)
 	socket = modbus_tcp_listen(ctx, 1);
 	modbus_tcp_accept(ctx, &socket);
 	printf("TCP connection started!\n");
-
+	begin = clock();
 	while(1)
+			
 			{
 
 			rc = modbus_receive(ctx, query);
-			if (rc >= 0)	{					
+			if (rc >= 0)	{
+										
 					modbus_reply(ctx, query, rc, mb_mapping);
 					printf("answer sent!\n");
-				   	}
+				   	printf("registers 0, 1 ,2 ,3 ,4 are %d, %d, %d, %d, %d \n", mb_mapping->tab_registers[0], mb_mapping->tab_registers[1], mb_mapping->tab_registers[2], mb_mapping->tab_registers[3], mb_mapping->tab_registers[4]);
+                                        }
 			else
 					{
 					// Connection closed by the client or server
 					printf("Con Closed.\n");
 					modbus_close(ctx); // close
+					end = clock();
+					double connection_duration =(double)(end - begin)/CLOCKS_PER_SEC;
+					printf("This connection lasted %f \n", connection_duration);
 					// immediately start waiting for another request again
 					modbus_tcp_accept(ctx, &socket);
 					}
@@ -86,7 +94,7 @@ int main(void)
 	int err;
 	
 	//keep this
-	mb_mapping = modbus_mapping_new(100,100,100,100);
+	mb_mapping = modbus_mapping_new(10,10,10,10);
 	if (mb_mapping == NULL)
 			{
 			fprintf(stderr, "Failed to allocate the mapping: %s\n", modbus_strerror(errno));
